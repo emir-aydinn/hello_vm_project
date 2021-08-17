5fb539a6ee42d38ebdf836b8c426498856e0338f
emir-aydinn <81522910+emir-aydinn@users.noreply.github.com>
2021-08-17 14:59:42 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,6 +5,7 @@
   systemctl start docker
   systemctl stop firewalld
   systemctl disable firewalld
-  
+  docker build -t flask-tutorial:latest /filess
+  docker run -d -p 5000:80 flask-tutorial
   
   
